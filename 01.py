adbba5df741a181ee842cb6d873d08704deb17a7
totopc <61189921+totopc@users.noreply.github.com>
2020-12-01 15:11:19 +0800

    Create 01.py

---
+++ 01.py
@@ -0,0 +1,27 @@
+from itertools import combinations_with_replacement
+import math 
+with open('input.txt', 'r') as reader:
+    myList=[int(l) for l in reader.read().split()]
+
+n=2
+n2=3
+
+def sumTwo(listName):
+    for number in combinations_with_replacement (listName[::-1], n):
+  
+        if sum(number) == 2020:
+            results =math.prod(number[::-1])
+            return results
+
+        
+def sumThree(listName):
+    for number in combinations_with_replacement(listName[::-1],n2):
+        if sum(number)==2020:
+            results=math.prod(number[::-1])
+            return results
+        
+print (sumTwo(myList))
+print (sumThree(myList))
+
+
+
